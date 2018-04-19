c2d2af9d44a4300a90b37b64a54440663bffdba6
Edward A Maxedon <edadma@gmail.com>
2018-04-18 21:31:26 -0400

    code remaining conversions

--- src/main/scala/Strftime.scala
+++ src/main/scala/Strftime.scala
@@ -31,19 +31,52 @@ object Strftime {
 								appendText( MINUTE_OF_HOUR ).appendLiteral( ':' ).
 								appendText( SECOND_OF_MINUTE ).appendLiteral( ' ' ).
 								appendText( YEAR )
-						case 'C' => sys.error( "not implemented" )
+						case 'C' => sys.error( "not implemented" )	// todo: don't know how to do this
 						case 'd' => builder.appendValue( DAY_OF_MONTH, 2 )
 						case 'D' =>
 							builder.appendValue(MONTH_OF_YEAR, 2).appendLiteral('/').
 								appendValue( DAY_OF_MONTH, 2 ).appendLiteral('/').
 								appendValueReduced( YEAR, 2, 2, 2000 )
+						case 'e' => sys.error( "not implemented" )  // todo
+						case 'F' =>
+							builder.appendText( YEAR ).appendLiteral( '-' ).
+								appendValue( MONTH_OF_YEAR, 2 ).appendLiteral( '-' ).
+								appendValue( DAY_OF_MONTH, 2 )
+						case 'g' => sys.error( "not implemented" )	// todo
+						case 'G' => sys.error( "not implemented" )	// todo
+						case 'h' => builder.appendText( MONTH_OF_YEAR, SHORT )
 						case 'H' => builder.appendText( HOUR_OF_DAY )
 						case 'I' => builder.appendText( HOUR_OF_AMPM )
+						case 'j' => builder.appendText( DAY_OF_YEAR )
 						case 'm' => builder.appendValue( MONTH_OF_YEAR, 2 )
 						case 'M' => builder.appendText( MINUTE_OF_HOUR )
+						case 'n' => builder.appendLiteral( '\n' )
+						case 'p' => builder.appendText( AMPM_OF_DAY )
+						case 'r' =>
+							builder.appendText( HOUR_OF_AMPM ).appendLiteral( ':' ).
+								appendText( MINUTE_OF_HOUR ).appendLiteral( ':' ).
+								appendText( SECOND_OF_MINUTE ).appendLiteral( ' ' ).
+								appendText( AMPM_OF_DAY )
+						case 'R' =>
+							builder.appendText( HOUR_OF_DAY ).appendLiteral( ':' ).
+								appendText( MINUTE_OF_HOUR )
 						case 'S' => builder.appendText( SECOND_OF_MINUTE )
+						case 't' => builder.appendLiteral( '\t' )
+						case 'T' =>
+							builder.appendText( HOUR_OF_DAY ).appendLiteral( ':' ).
+								appendText( MINUTE_OF_HOUR ).appendLiteral( ':' ).
+								appendText( SECOND_OF_MINUTE )
+						case 'u' => sys.error( "not implemented" )  // todo
+						case 'U' => sys.error( "not implemented" )  // todo
+						case 'V' => sys.error( "not implemented" )  // todo
+						case 'w' => sys.error( "not implemented" )  // todo
+						case 'W' => sys.error( "not implemented" )  // todo
+						case 'x' => sys.error( "not implemented" )  // todo
+						case 'X' => sys.error( "not implemented" )  // todo
 						case 'y' => builder.appendValueReduced( YEAR, 2, 2, 2000 )
 						case 'Y' => builder.appendText( YEAR )
+						case 'z' => builder.appendZoneOrOffsetId
+						case 'Z' => builder.appendZoneId
 					}
 				case c => builder.appendLiteral( c )
 			}
--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -8,12 +8,14 @@ import java.time.format.TextStyle._
 
 object Main extends App {
 
-	val builder = new DateTimeFormatterBuilder
+//	val builder = new DateTimeFormatterBuilder
+//
+//	builder.appendValue( YEAR, 2 )
+//
+//	val formatter = builder.toFormatter
+//
+//	println( formatter.format( LocalDateTime.now ) )
 
-	builder.appendValue( YEAR, 2 )
-
-	val formatter = builder.toFormatter
-
-	println( formatter.format( LocalDateTime.now ) )
+	println( Strftime.format("%z") )
 
 }
